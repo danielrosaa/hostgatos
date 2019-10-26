984a24d0da3ce409fc34d53b6ef551b309d8d805
Daniel Rosa <danielrosaa@gmail.com>
2019-10-26 13:36:11 -0300

    Created components for Header and Sidenav.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import './App.scss';
-import Avatar from './assets/images/avatar.png'
-
-// FontAwesome
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons' 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Sidenav from './components/Sidenav'
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
-      <div className="sidenav">
-        Side Nav
-      </div>
-      <header>
-        <div className="header-content">
-          <div className="title">Breeds</div>
-          <div className="user">
-            <img src={Avatar} alt="Avatar Icon"/>
-            <FontAwesomeIcon icon={faAngleDown} />
-          </div>
-        </div>
-      </header>
+      <Sidenav />
+      <Header />
       <div className="content">
         Content
       </div>
---
+++ src/components/Sidenav.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './Sidenav.scss'
+
+class Sidenav extends Component {
+    render() {
+        return (
+            <div className='sidenav'>Side Nav</div>
+        )
+    }
+}
+
+export default Sidenav
