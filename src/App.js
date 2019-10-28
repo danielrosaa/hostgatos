eaeb24588f6162a2ef7cfddaf14f073314e81e08
Daniel <drosa@indracompany.com>
2019-10-28 10:58:02 -0300

    Fix test.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.scss';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers/rootReducers'
+
 import Sidenav from './components/sidenav'
 import Header from './components/header'
 import Content from './components/content'
 
+const store = createStore(rootReducer)
+
 function App() {
   return (
     <div>
-      <Sidenav />
-      <Header />
-      <Content />
+      <Provider store={store}>
+        <Sidenav />
+        <Header />
+        <Content />
+      </Provider>
     </div>
   );
 }
