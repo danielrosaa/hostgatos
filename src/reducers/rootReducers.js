80324a2503375b08f44f4b8237f359a8e3398c7d
Daniel Rosa <danielrosaa@gmail.com>
2019-10-27 22:34:55 -0300

    Remove logs from rootReducers.

--- src/reducers/rootReducers.js
+++ src/reducers/rootReducers.js
@@ -10,14 +10,12 @@ const initialState = {
 export default function(state = initialState, action) {
     switch(action.type) {
         case FETCH_DATA:
-            console.log('payload FETCH_DATA', action.payload)
             return {
                 ...state,
                 breed: action.payload,
                 resultNumber: action.payload.length
             }
         case FETCH_IMAGE:
-            console.log('payload FETCH_IMAGE', action.payload)
             return {
                 ...state,
                 image: action.payload
