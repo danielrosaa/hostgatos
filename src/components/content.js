fbdbf19a1155765e5cab3b00fe888120d926c987
Daniel Rosa <danielrosaa@gmail.com>
2019-10-28 23:50:37 -0300

    Code impromements.

--- src/components/content.js
+++ src/components/content.js
@@ -27,18 +27,57 @@ class Content extends Component {
             })
         }
     }
-
     handleLoadMore(e) {
         e.preventDefault();
         this.setState({ show: true, showMore: true })
     }
 
-    getMoreValues() {
+    showResults() {
+        if (this.state.show) {
+            if (this.props.resultNumber === 0) {
+                return <h3>No data found</h3>
+            } else {
+                return <Result
+                    breed={this.props.breed[0].name}
+                    description={this.props.breed[0].description}
+                    affection_level={this.props.breed[0].affection_level}
+                    adaptability={this.props.breed[0].adaptability}
+                    dog_friendly={this.props.breed[0].dog_friendly}
+                    child_friendly={this.props.breed[0].child_friendly}
+                    search={this.state.search}
+                    img={this.props.breed[0].id}
+                />
+            }
+        }
+    }
+
+    showMoreResults() {
+        if (this.state.showMore) {
+            let otherResults = this.props.breed.slice(1).map((value, i) => {
+                return <Result key={value.id} breed={value.name} search={this.state.search} description={value.description} img={value.id}
+                    affection_level={this.props.breed[i].affection_level}
+                    adaptability={this.props.breed[i].adaptability}
+                    dog_friendly={this.props.breed[i].dog_friendly}
+                    child_friendly={this.props.breed[i].child_friendly} />
+            })
+            return otherResults
+        }
+    }
+
+    showLoadMoreButton() {
         if (this.props.breed.length > 1) {
             if (this.state.show && !this.state.showMore) {
                 return <button type="button" onClick={(e) => this.handleLoadMore(e)}>Load more</button>
             }
         }
+    }
+
+    showResultCount() {
+        if (this.state.show) {
+            return <div className="found">
+                        <span>{this.props.resultNumber > 0 ? this.props.resultNumber : 0} result(s) found</span>
+                    </div>
+        } 
     }
 
     render() {
@@ -57,38 +96,15 @@ class Content extends Component {
                             onChange={e => this.setState({ search: e.target.value })} />
                     </div>
                     <div className="results-found">
-                        {this.props.resultNumber !== -1 ?
-                            <div className="found">
-                                <span>{this.props.resultNumber > 0 ? this.props.resultNumber : 0} result(s) found</span>
-                            </div>
-                            : ''
-                        }
-                        {this.props.resultNumber === 0
-                            ? <h3>No data found</h3>
-                            : this.state.show ?
-                                <Result
-                                    breed={this.props.breed[0].name}
-                                    description={this.props.breed[0].description}
-                                    affection_level={this.props.breed[0].affection_level}
-                                    adaptability={this.props.breed[0].adaptability}
-                                    dog_friendly={this.props.breed[0].dog_friendly}
-                                    child_friendly={this.props.breed[0].child_friendly}
-                                    search={this.state.search}
-                                    img={this.props.breed[0].id}
-                                     />
-                                : ''}
-                        {this.state.showMore &&
-                            this.props.breed.slice(1).map((value, i) => {
-                                    return <Result key={value.id} breed={value.name} search={this.state.search} description={value.description} img={value.id}
-                                                affection_level={this.props.breed[i].affection_level}
-                                                adaptability={this.props.breed[i].adaptability}
-                                                dog_friendly={this.props.breed[i].dog_friendly}
-                                                child_friendly={this.props.breed[i].child_friendly}/>
-                            })
-                        }
+                        
+                        {this.showResultCount()}
+
+                        {this.showResults()}
+
+                        {this.showMoreResults()}
 
                         <div className='load-more'>
-                            {this.getMoreValues()}
+                            {this.showLoadMoreButton()}
                         </div>
                     </div>
 
